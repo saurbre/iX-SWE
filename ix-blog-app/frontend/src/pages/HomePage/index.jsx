7e4e56fb46d600539be1d21a4e01bad287d392ea
saurbre <saurbre@umich.edu>
2024-07-17 12:05:54 +0200

    made homePage have data from byron's link

--- ix-blog-app/frontend/src/pages/HomePage/index.jsx
+++ ix-blog-app/frontend/src/pages/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import BlogGrid from "../../components/BlogGrid";
 import CategoriesList from "../../components/CategoryList";
@@ -6,12 +6,36 @@ import Footer from "../../components/Footer";
 import Heading from "../../components/Heading";
 import Navbar from "../../components/Navbar";
 import Subheading from "../../components/SubHeading";
+import Loader from "../../components/Loader";
 
-const data = require("../../dummy-data.json");
-const blogs = data.blogPosts.reverse();
-const categories = data.categories;
+import blogsService from "../../services/blogsService";
+import categoriesService from "../../services/categoryService";
+
+// const data = require("../../dummy-data.json");
+// const blogs = data.blogPosts.reverse();
+// const categories = data.categories;
 
 export default function HomePage() {
+  const [blogs, setBlogs] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      const blogRes = await blogsService.getBlogs();
+      const catRes = await categoriesService.getCategories();
+      setBlogs(blogRes.data);
+      setCategories(catRes.data);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <>
       <Navbar />
